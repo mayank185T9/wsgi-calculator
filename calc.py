43d6feafd503711706f33c61c4708e18d91a8fa8
mayank185T9 <43036566+mayank185T9@users.noreply.github.com>
2018-10-03 22:18:10 +0530

    its a simple one but it works

---
+++ calc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def calc(x, y, z):
+   if(z==1):
+       print(x+y)
+   elif(z==2):
+       print(x-y)
+   elif(z==3):
+       print(x*y)
+   elif(z==4):
+       print(x/y)
+   else:
+       print('not an operation')
+       
+   
+    
+    
+"""
+Created on Wed Sep 19 19:51:11 2018
+
+@author: mayank
+"""
+
